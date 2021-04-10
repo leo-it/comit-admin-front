05a8b8fac713eb5ddf264c9be4bd385c039eaedd
Leonardo Sainz <leo.database92@gmail.com>
2021-04-10 03:03:09 -1200

    formulario con card que se actualiza a medida que escribis

---
+++ assets/js/upload.js
@@ -0,0 +1,25 @@
+let inputTitulo = document.getElementById("title-admin");
+const tituloCard = document.getElementById('titulo-card')
+let inputImg = document.getElementById("img-admin");
+let imagen = document.getElementById('imgCard')
+
+
+inputTitulo.addEventListener('keyup', (e) => {
+let valorInput = e.target.value
+ inputTitulo = valorInput
+ //elimina el ultimo espaciiado
+.trim();
+tituloCard.textContent = valorInput
+})
+
+
+inputImg.addEventListener('keyup', (e) => {
+    let valorInput2 = e.target.value
+     inputTitulo = valorInput2
+     //elimina el ultimo espaciiado
+    .trim();
+    //imgCard.textContent = valorInput2
+    //const imagen = document.createElement('img')
+
+    imagen.src = valorInput2
+    })
